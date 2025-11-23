570589f2ee790c7e8ab7cbfd5163a656c0c890ba
ShutSV <132217445+ShutSV@users.noreply.github.com>
2025-11-23 23:03:48 +0300

    check_cuda_in_host

--- build_and_run.sh
+++ build_and_run.sh
@@ -10,6 +10,7 @@ docker buildx build --platform linux/amd64 -t 2025-11-win-train-ocr-server .
 # Запуск контейнера
 echo "Запуск контейнера..."
 # перед запуском контейнера проверить доступность видеокарты (в тч можно 'nvcc --version' для rtx4000ada cuda 12.8
+# для этого выключить Atomman, вынуть из него шнур питания на 5 сек, включить и загрузить, затем перезагрузить, и затем:
 nvidia-smi
 
 docker run -d --name ocr-server -p 8000:8000 --restart unless-stopped 2025-11-win-train-ocr-server
