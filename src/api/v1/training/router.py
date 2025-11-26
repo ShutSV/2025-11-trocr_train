645a4abf9ec3afee0c91959fa51b3bb8b9ec19c4
ShutSV <132217445+ShutSV@users.noreply.github.com>
2025-11-26 21:24:59 +0300

    Update router.py

--- src/api/v1/training/router.py
+++ src/api/v1/training/router.py
@@ -3,6 +3,7 @@ from fastapi.responses import ORJSONResponse
 from .config import router as config_router
 from .health import router as health_router
 from .start import router as start_router
+from .stop import router as stop_router
 from .status import router as status_router
 
 
@@ -13,4 +14,5 @@ router = APIRouter(
 router.include_router(router=config_router)
 router.include_router(router=health_router)
 router.include_router(router=start_router)
+router.include_router(router=stop_router)
 router.include_router(router=status_router)
