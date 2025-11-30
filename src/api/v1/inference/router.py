be7b0e6a17ed5a82ecf5c1f24c888c3a368011a6
ShutSV <132217445+ShutSV@users.noreply.github.com>
2025-11-30 15:01:45 +0300

    purge

--- src/api/v1/inference/router.py
+++ src/api/v1/inference/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import ORJSONResponse
 from .config import router as config_router
-from .health import router as health_router
 from .ocr import router as ocr_router
 
 
@@ -10,5 +9,4 @@ router = APIRouter(
     default_response_class=ORJSONResponse
 )
 router.include_router(router=config_router)
-router.include_router(router=health_router)
 router.include_router(router=ocr_router)
